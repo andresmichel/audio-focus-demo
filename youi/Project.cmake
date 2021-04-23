89d631087dfd90123aedb1cb522bcef8ef684d1e
Andres Michel <andresmichelg@gmail.com>
2021-04-23 13:36:23 -0700

    Update package name

--- youi/Project.cmake
+++ youi/Project.cmake
@@ -1,3 +1,3 @@
 set(YI_PROJECT_NAME "audio-focus-demo") # The name of the project. When building for Android, this is passed from Android Studio.
-set(YI_PACKAGE_NAME "tv.youi.audio-focus-demo") # The package name used for the project.")
+set(YI_PACKAGE_NAME "tv.youi.audiofocusdemo") # The package name used for the project.")
 set(YI_DISPLAY_NAME "audio-focus-demo") # The name of the application displayed on the device.
